6830d6f6cd6d227e8c7eee93692de6cfe304b96e
yacut <yacut@protonmail.com>
2017-03-19 22:51:30 +0100

    better samples

--- test/all-in-one.js
+++ test/all-in-one.js
@@ -1,107 +1,36 @@
 const assert = require('assert');
 
-describe('mocha runner', () => {
-  before('Before all hook', (done) => {
-    console.info('before all');
-        // throw new Error('whatever');
-    done();
-  });
-
-  it('should start log', (done) => {
-    console.info('Hello');
-    done();
-  });
-
-  it('should throw', () => {
-    console.error(new Error('test#1'));
-    console.log('sgsdglknsldgnslkdgnlasgndl;asdgnlasdgnasldg0');
-  });
-
-  it('should return console log text and shows strings diff', () => {
-    console.info('test#string');
+describe('Some mocha tests', () => {
+  it('should compare strings', () => {
     assert.equal('stringA', 'stringB');
   });
 
-  it('should return console log text and shows date diff', () => {
-    console.info('test#date');
+  it.skip('should skip test', () => {
+    console.info('no console log on skipped test');
+  });
+
+  it('should compare dates', () => {
     assert.equal(new Date(), new Date('01-01-2016'));
   });
 
-  it('should return console log text and shows array diff', () => {
-    console.info('test#array');
+  it('should compare arrays', () => {
     assert.equal(['1', '2'], ['3', '2']);
   });
 
   it('should compare two objects', () => {
-    console.info('test#object');
-    const foo = {
-      largestCities: [
-        'São Paulo',
-        'Buenos Aires',
-        'Rio de Janeiro',
-        'Lima',
-        'Bogotá',
-      ],
-      languages: [
-        'spanish',
-        'portuguese',
-        'english',
-        'dutch',
-        'french',
-        'quechua',
-        'guaraní',
-        'aimara',
-        'mapudungun',
-      ],
-    };
-    const bar = {
-      largestCities: [
-        'São Paulo',
-        'Buenos Aires',
-        'Rio de Janeiro',
-        'Lima',
-        'Bogotá',
-      ],
-      languages: [
-        'spanish',
-        'portuguese',
-        'inglés',
-        'dutch',
-        'french',
-        'quechua',
-        'guaraní',
-        'aimara',
-        'mapudungun',
-      ],
-    };
-    assert.deepEqual(foo, bar);
+    const foo = { foo: 1 };
+    const bar = { bar: 2 };
+    assert.equal(foo, bar);
   });
 
-  it('should compare true and false', () => {
+  it('should compare booleans', () => {
     assert.equal(true, false);
   });
 
-  describe('Handle console output deeper', () => {
+  describe('Some second level describe', () => {
     it('should delay test and pass', (done) => {
-      console.info('test with delay');
       assert.ok(true, true);
-      setTimeout(done, 5000);
+      setTimeout(done, 500);
     });
-  });
-
-
-  describe('Handle console output1', () => {
-    it('should return console log text', () => {
-      console.info('test pass');
-    });
-  });
-
-  describe('Skip this', () => {
-    it.skip('should skip test', () => {
-      console.info('test skiped');
-    });
-  });
-  after('after this describe hook', () => {
-    console.info('after hook');
   });
 });
